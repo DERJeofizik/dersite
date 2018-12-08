d5115740847d76350a9605ded83a93ee3d72bb46
Ridvan Orsvuran <orsvuran@geoazur.unice.fr>
2018-12-08 13:48:39 -0700

    update submodule before pushing

--- dersite/deploy.py
+++ dersite/deploy.py
@@ -5,14 +5,17 @@ from . import utils
 import datetime
 
 
+def run_command(command):
+    print(command)
+    os.system(command)
+
+
 def deploy(conf):
     print("Updating files")
     utils.copy_files(str(conf.output_dir / "*"), conf.deploy_dir)
     os.chdir(conf.deploy_dir)
+    run_command("git pull origin master")
+    run_command("git checkout master")
     today = datetime.datetime.today()
-    commit_command = f"git commit -am '{today}'"
-    print(commit_command)
-    os.system(commit_command)
-    push_command = "git push origin master"
-    print(push_command)
-    os.system(push_command)
+    run_command(f"git commit -am '{today}'")
+    run_command("git push origin master")
