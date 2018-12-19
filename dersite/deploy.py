1dbed8af4e2dbae45bbb7168570d867f7259740a
Ridvan Orsvuran <orsvuran@geoazur.unice.fr>
2018-12-19 13:10:39 -0700

    add new files while updating the website.

--- dersite/deploy.py
+++ dersite/deploy.py
@@ -17,5 +17,6 @@ def deploy(conf):
     run_command("git pull origin master")
     run_command("git checkout master")
     today = datetime.datetime.today()
+    run_command("git add .")
     run_command(f"git commit -am '{today}'")
     run_command("git push origin master")
