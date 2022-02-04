ff58de36ef14a1a201c8651359d6f602f889a246
Ridvan Orsvuran <ridvan.orsvuran@gmail.com>
2022-02-04 22:41:05 +0300

    added html page functionality

--- dersite/create.py
+++ dersite/create.py
@@ -10,6 +10,40 @@ from . import utils
 from .config import Config
 from .template import WebsiteCreator
 
+def create_pages(conf: Config):
+    # Preparing pages
+    pages = {}
+    for page in conf.pages_dir.glob("*"):
+        name = page.stem
+        info = {"title": name, "subtitle": "",
+                "slug": utils.slugify(name.lower())}
+        if page.suffix == ".md":
+            info["filetype"] = "markdown"
+        elif page.suffix == ".html":
+            info["filetype"] = "html"
+        else:
+            # unsupported filetype
+            continue
+
+        with open(page) as f:
+            page_content = f.read()
+
+        info["content"] = page_content
+
+        if page_content.startswith("+++\n"):
+            if page_content.count("+++\n") < 2:
+                raise Exception("Page markdown toml front matter line found (+++) but ending couldn't be found.")
+            data = page_content.split("+++\n")
+            front_matter = utils.parse_toml(data[1])
+            info.update(front_matter)
+            # Maybe content has +++ as well, we can reintroduce them here.
+            info["content"] = "+++\n".join(data[2:])
+
+        pages[name] = info
+
+
+    return pages
+
 
 def create(conf: Config, data_filename, base_url=None):
     if base_url:
@@ -22,29 +56,8 @@ def create(conf: Config, data_filename, base_url=None):
             shutil.rmtree(path)
     os.makedirs(conf.output_dir, exist_ok=True)
 
-    c = WebsiteCreator(conf)
 
-    # Preparing pages
-    pages = {}
-    for page in conf.pages_dir.glob("*.md"):
-        with open(page) as f:
-            page_content = f.read()
-
-        name = page.stem
-        info = {"content": page_content, "title": name,
-                "subtitle": "", "slug": utils.slugify(name.lower())}
-
-        if page_content.startswith("+++\n"):
-            if page_content.count("+++\n") < 2:
-                raise Exception("Page markdown toml front matter line found (+++) but ending couldn't be found.")
-            data = page_content.split("+++\n")
-            front_matter = utils.parse_toml(data[1])
-            info.update(front_matter)
-            # Maybe content has +++ as well, we can reintroduce them here.
-            info["content"] = "+++\n".join(data[2:])
-
-        info["content"] = markdown.markdown(info["content"])
-        pages[name] = info
+    pages = create_pages(conf)
 
     videos_data = utils.load_youtube_data(data_filename, channel=True)
 
@@ -70,12 +83,12 @@ def create(conf: Config, data_filename, base_url=None):
         "list_url": "liste",
         "published_date": utils.get_cur_time()
     }
+    c = WebsiteCreator(conf, common_data)
 
     print("Creating index.html...")
     c.render_to_file("index.html",
                      conf.output_dir / "index.html",
-                     entries=videos_data["entries"][0]["entries"],
-                     **common_data)
+                     entries=videos_data["entries"][0]["entries"])
 
     video_dir = conf.output_dir / common_data["video_url"]
     os.makedirs(video_dir)
@@ -83,8 +96,7 @@ def create(conf: Config, data_filename, base_url=None):
     for entry in videos_data["entries"][0]["entries"]:
         c.render_to_file("video.html",
                          video_dir / f"{entry['slug']}.html",
-                         entry=entry,
-                         **common_data)
+                         entry=entry)
 
     list_dir = conf.output_dir / common_data["list_url"]
     os.makedirs(list_dir)
@@ -93,8 +105,7 @@ def create(conf: Config, data_filename, base_url=None):
         c.render_to_file("playlist.html",
                          list_dir / f"{entry['slug']}.html",
                          entries=entry["entries"],
-                         title=entry["title"],
-                         **common_data)
+                         title=entry["title"])
 
     print("Copying include files...")
     utils.copy_files(str(conf.include_dir / "*"), conf.output_dir)
@@ -104,6 +115,11 @@ def create(conf: Config, data_filename, base_url=None):
     os.makedirs(page_dir)
     for page, info in pages.items():
         url = page_dir / (info["slug"]+".html")
-        c.render_to_file("page.html", url,
-                         **common_data,
-                         **info)
+        if info["filetype"] == "markdown":
+            info["content"] = markdown.markdown(info["content"])
+            c.render_to_file("page.html", url,
+                             **info)
+        elif info["filetype"] == "html":
+            c.render_html_page(info, url)
+        else:
+            raise Exception(f"Unknown page file type: {info['filetype']}.")
--- dersite/template.py
+++ dersite/template.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from jinja2 import Environment, FileSystemLoader, select_autoescape
+from jinja2 import Environment, FileSystemLoader, select_autoescape, Template
 
 from . import utils
 
 
 class WebsiteCreator:
     """General class for website creation"""
-    def __init__(self, conf):
+    def __init__(self, conf, common_data=None):
         super().__init__()
         self.conf = conf
         self.env = Environment(
@@ -15,15 +15,24 @@ class WebsiteCreator:
             autoescape=select_autoescape(["html", "xml"]))
         self.env.filters["regex_replace"] = utils.regex_replace
 
+        if common_data is None:
+            common_data = {}
+        self.common_data = common_data
+        self.common_data["sitetitle"] = self.conf.title
+        self.common_data["base_url"] = self.conf.base_url
+
     def render_to_file(self, filename, target_filename, **data):
         """Render template using data"""
-        if len(data) == 0:
-            data = {}
-
-        data["sitetitle"] = self.conf.title
-        data["base_url"] = self.conf.base_url
-
         template = self.env.get_template(filename)
-        output = template.render(**data)
+        self.render_template(template, target_filename, **data)
+
+    def render_html_page(self, page_info, target_filename):
+        template = self.env.from_string(page_info["content"])
+        self.render_template(template, target_filename,
+                             authors=self.conf.authors,
+                             **page_info)
+
+    def render_template(self, template, target_filename, **data):
+        output = template.render(**self.common_data, **data)
         with open(target_filename, "w") as f:
             f.write(output)
